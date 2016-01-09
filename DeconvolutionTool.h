534231c8ea54b62ef8f465e4e7863658a05929f7
David Caratelli <devish.c@gmail.com>
2016-01-09 13:29:44 -0600

    updates -- David Caratelli

--- DeconvolutionTool.h
+++ DeconvolutionTool.h
@@ -37,6 +37,28 @@ namespace signalana{
     /// Default destructor
     ~DeconvolutionTool(){}
 
+    /**
+       @brief Prepare FFTW3
+       @detail This function prepares the tools needed by the FFTW3
+       package. Given N, the number of ticks in the input
+       signal, a plan for ft and i-ft are prepared for future use
+     */
+    void PrepareFFTW3(const int& N);
+
+    /**
+       @brief Reset FFTW3
+       @detail function to clear memory and reset all tools used
+       by fftw3
+     */
+    void ResetFFTW3();
+
+    /**
+    @brief Apply Deconvolution
+    @detail Take input signal and after checking the vector size is
+    appropriate then apply the deconvolution.
+    */
+    void Deconvolve(const std::vector<double>& sig_v);
+
 
   private:
 
@@ -56,6 +78,12 @@ namespace signalana{
     double *_signal;
     // _sigfft -> the fft of the original signal
     complex *_sigfft;
+    // _processed -> the output time-domain signal
+    // after processing
+    double *_processed;
+
+    // the size of the signal expected by this instance of DeconvolutionTool
+    int _N;
     
   };
 
